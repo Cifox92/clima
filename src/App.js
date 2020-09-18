afd8dbc757222ead5d654aa0460530af6161bad3
David <drgomez92@gmail.com>
2020-09-18 18:01:55 +0200

    Añadida interfaz con formulario. Conectado a la API. Añadido Materialize (CDN). Comprobación de info de entrada en el formulario y gestión de errores.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
+
+  const [consultar, guardarConsultar] = useState(false)
+
+  const {ciudad, pais} = busqueda
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if(consultar) {
+        //KEY de la aplicación y la url con los valores que se pasan en formato query string
+        const appId = 'deb1cf72487aa6baac8035d8ba76e829'
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+
+        //esperamos la respuesta de la API y la guardamos en formato JSON
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        console.log(resultado)
+      }
+    }
+
+    consultarAPI()
+  }, [consultar])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header titulo='Clima React App' />
+
+      <div className='contenedor-form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Formulario busqueda={busqueda} guardarBusqueda={guardarBusqueda} guardarConsultar={guardarConsultar} /> 
+            </div>
+            <div className='col m6 s12'>
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App

1fb915be793d65a62197bdde9123d16442a421f6
David <drgomez92@gmail.com>
2020-09-21 20:31:05 +0200

    Proyecto finalizado. Creado componente que muestra la respuesta de la API. Carga condicional de componentes en caso de que la API devuelva un error. Reestructuración del componente que muestra errores para mejorar su usabilidad.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
+import Clima from './components/Clima'
+import Error from './components/Error'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
   })
 
   const [consultar, guardarConsultar] = useState(false)
+  const [resultado, guardarResultado] = useState({})
+  const [error, guardarError] = useState(false) //En caso de que la API no encuentre la ciudad...
 
   const {ciudad, pais} = busqueda
 
@@ -24,12 +28,25 @@ function App() {
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
 
-        console.log(resultado)
+        guardarResultado(resultado)
+        guardarConsultar(false)
+        
+        //En caso de que la API devuelva un error
+        if(resultado.cod === '404') {
+          guardarError(true)
+        } else {
+          guardarError(false)
+        }
       }
     }
 
     consultarAPI()
+    // eslint-disable-next-line
   }, [consultar])
+
+
+  let componente
+  error ? componente = <Error mensaje='No hay resultados.' /> : componente = <Clima resultado={resultado} />
 
   return (
     <>
@@ -42,7 +59,7 @@ function App() {
               <Formulario busqueda={busqueda} guardarBusqueda={guardarBusqueda} guardarConsultar={guardarConsultar} /> 
             </div>
             <div className='col m6 s12'>
-              2
+              {componente}
             </div>
           </div>
         </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import Error from './Error'
+import PropTypes from 'prop-types'
 
 const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
@@ -26,7 +28,7 @@ const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
     return (
         <form onSubmit={handleSubmit}>
 
-            {error ? <p className='red darken-4 error'>Todos los campos son obligatorios</p> : null }
+            {error ? <Error mensaje='Todos los campos son obligatorios' /> : null}
 
             <div className='input-field col s12'>
                 <input type='text' name='ciudad' id='ciudad' value={ciudad} onChange={handleChange} />
@@ -52,6 +54,13 @@ const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
             </div>
         </form>
     )
+}
+
+
+Formulario.propTypes = {
+    busqueda: PropTypes.object.isRequired, 
+    guardarBusqueda: PropTypes.func.isRequired,
+    guardarConsultar: PropTypes.func.isRequired
 }
  
 export default Formulario
